cf18584c463e15842caac9b1126a83fa3c0458f4
Mikhail Romanenko <rmr04489@gmail.com>
2024-11-19 15:45:28 +0300

    Init commit

---
+++ src/test/java/com/brigada/carsh/CarshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigada.carsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
