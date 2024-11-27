86079519affaef5924901524fc1e0cbf7a7996c0
Mikhail Romanenko <rmr04489@gmail.com>
2024-11-27 23:36:37 +0300

    Some docs

--- src/main/java/com/brigada/carsh/controller/CarController.java
+++ src/main/java/com/brigada/carsh/controller/CarController.java
@@ -20,17 +20,20 @@ public class CarController {
     private final CarService carService;
 
     @GetMapping
+    @Operation(description = "Get all cars")
     public ResponseEntity<List<CarResponseDTO>> getAllCars() {
         return ResponseEntity.ok(carService.getAllCars());
     }
 
     @GetMapping("/available")
+    @Operation(description = "Get all available cars")
     public ResponseEntity<List<CarResponseDTO>> getAllAvailableCars() {
         return ResponseEntity.ok(carService.getAllAvailableCars());
     }
 
     @GetMapping("/by-radius")
     @Operation(
+            description = "Get all available cars by user lat, lng and radius in km",
             parameters = {
                     @Parameter(name = "latitude", description = "Широта", required = true, example = "60.078811"),
                     @Parameter(name = "longitude", description = "Долгота", required = true, example = "30.247230"),
@@ -42,16 +45,19 @@ public class CarController {
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Get car by id")
     public ResponseEntity<CarResponseDTO> getCarById(@PathVariable Long id) {
         return ResponseEntity.ok(carService.getCarById(id));
     }
 
     @PostMapping
+    @Operation(description = "Add car")
     public ResponseEntity<CarResponseDTO> addCar(@RequestBody CarRequestDTO carRequestDTO) {
         return new ResponseEntity<>(carService.addCar(carRequestDTO), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(description = "Update car by id")
     public ResponseEntity<CarResponseDTO> updateCar(@RequestBody CarRequestDTO carRequestDTO, @PathVariable Long id) {
         return ResponseEntity.ok(carService.updateCar(carRequestDTO, id));
     }
--- src/main/java/com/brigada/carsh/controller/MaintenanceController.java
+++ src/main/java/com/brigada/carsh/controller/MaintenanceController.java
@@ -3,6 +3,7 @@ package com.brigada.carsh.controller;
 import com.brigada.carsh.dto.request.MaintenanceRequestDTO;
 import com.brigada.carsh.dto.response.MaintenanceResponseDTO;
 import com.brigada.carsh.service.MaintenanceService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,26 +18,31 @@ public class MaintenanceController {
     private final MaintenanceService maintenanceService;
 
     @GetMapping("/car/{carId}")
+    @Operation(description = "Get maintenance by car")
     public ResponseEntity<List<MaintenanceResponseDTO>> getAllMaintenanceByCarId(@PathVariable Long carId) {
         return ResponseEntity.ok(maintenanceService.getAllMaintenanceByCarId(carId));
     }
 
     @PostMapping("/car/{carId}")
+    @Operation(description = "Add maintenance to car")
     public ResponseEntity<MaintenanceResponseDTO> addMaintenance(@RequestBody MaintenanceRequestDTO maintenanceRequestDTO, @PathVariable Long carId) {
         return new ResponseEntity<>(maintenanceService.addMaintenance(maintenanceRequestDTO, carId), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(description = "Update maintenance")
     public ResponseEntity<MaintenanceResponseDTO> updateMaintenance(@RequestBody MaintenanceRequestDTO maintenanceRequestDTO, @PathVariable Long id) {
         return ResponseEntity.ok(maintenanceService.updateMaintenance(maintenanceRequestDTO, id));
     }
 
     @PutMapping("/end/{id}")
+    @Operation(description = "End maintenance by id")
     public ResponseEntity<MaintenanceResponseDTO> endMaintenance(@PathVariable Long id) {
         return ResponseEntity.ok(maintenanceService.endMaintenance(id));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(description = "Delete maintenance by id")
     public ResponseEntity<Void> deleteMaintenance(@PathVariable Long id) {
         maintenanceService.deleteMaintenance(id);
         return ResponseEntity.noContent().build();
